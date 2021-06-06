28ab13e7f0d86085730b63607012f646037f185b
Nicholaos Bilalis <nikos.bilalis@gmail.com>
2021-06-06 22:47:12 +0300

    Change view function to use DB

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from os import environ
-from random import randint, choice, sample
+from random import randint, choice
 from inspect import cleandoc
-from flask import Flask, g, render_template
+from pathlib import Path
+from flask import Flask, g, request, render_template, abort
+import sqlite3
 
 app = Flask(__name__)
 
+app.secret_key = environ.get('SECRET_KEY', '1234')
 
 DATABASE_PATH = Path(__file__).parent / 'data/flask_shop.db'
 
@@ -72,22 +75,68 @@ def home():
 
 @app.route('/products/<category_id>')
 def product_list(category_id):
-    category = get_category(category_id, get_all_categories())
-    products = get_all_products()
-    app.logger.debug(products)
+    params = {'category_id': category_id}
+
+    cur = get_conn().cursor()
+    category = cur.execute(
+        '''
+        SELECT [id], [title], [parent]
+        FROM [product_category]
+        WHERE [id] = :category_id
+        ''', params
+    ).fetchone()
+    cur.close()
+
+    if not category:
+        abort(404)
+
+    cur = get_conn().cursor()
+    products = cur.execute(
+        '''
+        SELECT [id], [title], [description], [price], [discount_ratio], [stock], [is_hot]
+        FROM [product]
+        WHERE [product_category_id] = :category_id
+        ''', params
+    ).fetchall()
+    cur.close()
+
+    if not products:
+        abort(404)
+
     return render_template('products/list.html', category=category, products=products)
 
 
-@app.route('/products/<category_id>/<product_id>')
+@app.route('/products/<category_id>/<int:product_id>')
 def product_details(category_id, product_id):
-    category = get_category(category_id, get_all_categories())
-    product = get_product(product_id)
+    cur = get_conn().cursor()
+    category = cur.execute(
+        '''
+        SELECT [id], [title], [parent]
+        FROM [product_category]
+        WHERE [id] = :category_id
+        ''',
+        {'category_id': category_id}
+    ).fetchone()
+    cur.close()
+
+    if not category:
+        abort(404)
+
+    cur = get_conn().cursor()
+    product = cur.execute(
+        '''
+        SELECT *
+        FROM [product]
+        WHERE [id] = :product_id
+        ''',
+        {'product_id': product_id}
+    ).fetchone()
+    cur.close()
+
+    if not product:
+        abort(404)
+
     return render_template('products/details.html', category=category, product=product)
-
-
-@app.route('/products/<product_id>/api')
-def get_product_json(product_id):
-    return get_product(product_id)
 
 
 @app.errorhandler(404)
@@ -96,182 +145,70 @@ def page_not_found(e):
     return render_template('errors/404.html'), 404
 
 
-"""
-Here be dragons!
-Please ignore this section... or don't!
-"""
+'''
+Here be dragons
+'''
 
 
-@app.before_request
-def store_stuff_in_g():
-    """
-    Executes before *every* request
-    g is a global object to store stuff for a *single* request
-    """
-    g.products = get_all_products()
-    g.categories = get_all_categories()
+''' @app.post('/products/<category_id>')
+def add_to_cart(category_id):
+    product_id = request.form.get('product-id')
+    product = get_product(product_id)
+
+    if not product:
+        abort(500)
+
+    cart = session.get('cart', [])
+    cart.append([product, 1])
+
+    app.logger.debug(cart)
+    session['cart'] = cart
+
+    return redirect(url_for('product_details', category_id=category_id, product_id=product_id)) '''
 
 
-def get_category(id, categories):
-    for category in categories:
-        if id == category['id']:
-            return category
-        elif 'children' in category:
-            if res := get_category(id, category['children']):
-                return res
-    else:
-        return None
+def insert_test_products(category):
+    adjectives = (
+        'Animal Print',
+        'Awesome',
+        'Casual',
+        'Formal',
+        'Gorgeous',
+        'Floral',
+        'Simple',
+        'Standard',
+        'Super',
+        'Stunning',
+        'Unique',
+    )
 
-
-def get_product(id):
-    adjectives = ('Super', 'Awesome', 'Unique', 'Gorgeous',
-                  'Floral', 'Casual', 'Standard', 'Formal')
-    items = ('Shirt', 'T-Shirt', 'Pants', 'Dress',
-             'Blouse', 'Jeans', 'Trousers')
     sizes = ('XS', 'S', 'M', 'L', 'XL')
     colors = ('green', 'yellow', 'pink', 'black', 'white')
 
-    description = cleandoc('''Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    description = cleandoc(
+        '''Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         Numquam accusamus facere iusto,
         autem soluta amet sapiente ratione inventore nesciunt a,
-        maxime quasi consectetur, rerum illum.''').replace('\n', '')
+        maxime quasi consectetur, rerum illum.'''
+    ).replace('\n', '')
 
-    is_sale = choice((True, False, False))
-    is_sold_out = not is_sale and choice((True, False, False))
-    is_hot = not is_sale and not is_sold_out and choice((True, False, False))
-
-    return {
-        'id': id,
-        'title': f'{choice(adjectives)} {choice(items)}',
-        'description': description,
-        'long_description': ' '.join([description for i in range(10)]),
-        'price': randint(1000, 9999) / 100,
-        'discount_ratio': randint(0, 99) / 100 if is_sale else 0,
-        'stock': randint(0, 100),
-        'sizes': sample(sizes, randint(1, len(sizes))),
-        'colors': sample(colors, randint(1, len(colors))),
-        'is_sale': is_sale,
-        'is_sold_out': is_sold_out,
-        'is_hot': is_hot
-    }
-
-
-def get_all_products(n=12):
-    return [get_product(f'{i:04}') for i in range(1, n+1)]
-
-
-def get_all_categories():
-    return [
-        {
-            'id': 'men',
-            'title': 'Men',
-            'children': [
-                {'id': 'casual', 'title': 'Casual'},
-                {'id': 'sports', 'title': 'Sports'},
-                {'id': 'formal', 'title': 'Formal'},
-                {'id': 'standard', 'title': 'Standard'},
-                {'id': 't-shirts', 'title': 'T-Shirts'},
-                {'id': 'shirts', 'title': 'Shirts'},
-                {'id': 'jeans', 'title': 'Jeans'},
-                {'id': 'trousers', 'title': 'Trousers'},
-                {
-                    'id': 'other',
-                    'title': 'And more…',
-                    'children': [
-                        {'id': 'sleep-wear', 'title': 'Sleep Wear'},
-                        {'id': 'sandals', 'title': 'Sandals'},
-                        {'id': 'loafers', 'title': 'Loafers'}
-                    ]
-                }
-            ]
-        },
-        {
-            'id': 'women',
-            'title': 'Women',
-            'children': [
-                {'id': 'kurta-n-kurti', 'title': 'Kurta & Kurti'},
-                {'id': 'trousers', 'title': 'Trousers'},
-                {'id': 'casual', 'title': 'Casual'},
-                {'id': 'sports', 'title': 'Sports'},
-                {'id': 'formal', 'title': 'Formal'},
-                {'id': 'sarees', 'title': 'Sarees'},
-                {'id': 'shoes', 'title': 'Shoes'},
-                {
-                    'id': 'other',
-                    'title': 'And more…',
-                    'children': [
-                        {'id': 'sleep-wear', 'title': 'Sleep Wear'},
-                        {'id': 'sandals', 'title': 'Sandals'},
-                        {'id': 'loafers', 'title': 'Loafers'},
-                        {
-                            'id': 'other',
-                            'title': 'And more…',
-                            'children': [
-                                {'id': 'Rings', 'title': 'Rings'},
-                                {'id': 'Earrings', 'title': 'Earrings'},
-                                {'id': 'Jewellery Sets', 'title': 'Jewellery Sets'},
-                                {'id': 'Lockets', 'title': 'Lockets'},
-                                # <li class="disabled"><a class="disabled" href="#">Disabled item</a></li>
-                                {'id': 'Jeans', 'title': 'Jeans'},
-                                {'id': 'Polo T-Shirts', 'title': 'Polo T-Shirts'},
-                                {'id': 'SKirts', 'title': 'SKirts'},
-                                {'id': 'Jackets', 'title': 'Jackets'},
-                                {'id': 'Tops', 'title': 'Tops'},
-                                {'id': 'Make Up', 'title': 'Make Up'},
-                                {'id': 'Hair Care', 'title': 'Hair Care'},
-                                {'id': 'Perfumes', 'title': 'Perfumes'},
-                                {'id': 'Skin Care', 'title': 'Skin Care'},
-                                {'id': 'Hand Bags', 'title': 'Hand Bags'},
-                                {'id': 'Single Bags', 'title': 'Single Bags'},
-                                {'id': 'Travel Bags', 'title': 'Travel Bags'},
-                                {'id': 'Wallets & Belts',
-                                    'title': 'Wallets & Belts'},
-                                {'id': 'Sunglases', 'title': 'Sunglases'},
-                                {'id': 'Nail', 'title': 'Nail'},
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            'id': 'kids',
-            'title': 'Kids',
-            'children': [
-                {'id': 'casual', 'title': 'Casual'},
-                {'id': 'sports', 'title': 'Sports'},
-                {'id': 'formal', 'title': 'Formal'},
-                {'id': 'standard', 'title': 'Standard'},
-                {'id': 't-shirts', 'title': 'T-Shirts'},
-                {'id': 'shirts', 'title': 'Shirts'},
-                {'id': 'jeans', 'title': 'Jeans'},
-                {'id': 'trousers', 'title': 'Trousers'},
-                {
-                    'id': 'other',
-                    'title': 'And more…',
-                    'children': [
-                        {'id': 'sleep-wear', 'title': 'Sleep Wear'},
-                        {'id': 'sandals', 'title': 'Sandals'},
-                        {'id': 'loafers', 'title': 'Loafers'}
-                    ]
-                }
-            ]
-        },
-        {'id': 'sports', 'title': 'Sports'},
-        {
-            'id': 'digital',
-            'title': 'Digital',
-            'children': [
-                {'id': 'camera', 'title': 'Camera'},
-                {'id': 'mobile', 'title': 'Mobile'},
-                {'id': 'tablet', 'title': 'Tablet'},
-                {'id': 'laptop', 'title': 'Laptop'},
-                {'id': 'accesories', 'title': 'Accesories'},
-            ]
-        },
-        {'id': 'furniture', 'title': 'Furniture'}
-    ]
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.executemany(
+        '''
+        INSERT INTO [product] ([title], [description], [price], [discount_ratio], [stock], [is_hot], [product_category_id])
+        VALUES (:title, :description, :price, :discount_ratio, :stock, :is_hot, :product_category_id)
+        ''',
+        ({'title': f"{adj} {category['title']}",
+         'description': description,
+          'price': randint(1000, 9999) / 100,
+          'discount_ratio': 0 if choice((True, False, False)) else randint(0, 80) / 100,
+          'stock': randint(0, 100),
+          'is_hot': choice((True, False, False)),
+          'product_category_id': category['id']} for adj in adjectives)
+    )
+    conn.commit()
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=environ.get('SERVER_PORT', 5000))
+    app.run(host='127.0.0.1', port=int(environ.get('SERVER_PORT', 5000)))
